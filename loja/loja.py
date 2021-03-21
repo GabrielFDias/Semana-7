5deddf1e9f3ccc7460a2d6ee6d461d3f62aa5599
GabrielFDias <g.dias_007@alunos.fho.edu.br>
2021-03-21 16:59:33 -0300

    Atividade da semana 7

---
+++ loja/loja.py
@@ -0,0 +1,21 @@
+from loja.classes.pedido import Pedido
+from loja.classes.cliente import Cliente
+from loja.classes.ecommerce import Loja
+
+
+loja = Loja('Loja Napp')
+loja.add_estoque('123', 15, 10)
+loja.add_estoque('1234', 20, 5)
+pedido = Pedido(Cliente('José da Silva'))
+cliente = Cliente('John Doe')
+pedido2 = Pedido(cliente)
+
+pedido.add_item(loja.comprar('1234'))
+pedido.add_item(loja.comprar('123'))
+pedido.add_item(loja.comprar('1234'))
+pedido.add_item(loja.comprar('123'))
+pedido2.add_item(loja.comprar('1234'))
+pedido2.add_item(loja.comprar('123'))
+
+loja.devolver_carrinho(pedido)
+pedido2.checkout('dinheiro')
